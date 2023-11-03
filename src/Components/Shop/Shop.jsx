6c38e5b51ede1fcfc9b6364a245e026a61d551ed
taslimaakterpapia <taslimaakterpapia495@gmail.comm>
2023-11-04 03:53:25 +0600

    find product quantity

--- src/Components/Shop/Shop.jsx
+++ src/Components/Shop/Shop.jsx
@@ -12,12 +12,28 @@ const Shop = () => {
     fetch('products.json')
       .then(res => res.json())
       .then(data => setProducts(data))
-  }, [])
+  }, []);
 
   useEffect(() => {
     const storedCart = getShoppingCart();
-    console.log(storedCart);
-  },[])
+    const savedCart = [];
+    // step 1: get id
+    for ( const id in storedCart){
+
+      // step 2: get the product by using id
+      const addedProduct = products.find(product => product.id === id);
+
+      if(addedProduct){
+        // step 3: get quantity of the product
+       const quantity = storedCart[id];
+       addedProduct.quantity =quantity;
+       // step 4: add the added product to the save cart 
+       savedCart.push(addedProduct);
+      }
+
+    }
+    setCart(savedCart);
+  },[products])
 
   const handlAaddToCart = (product) => {
     const newCart = [...cart, product];
